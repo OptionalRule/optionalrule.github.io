ef6765804f9fc895b8a3fc97f0e859625ca6cc06
Scott Turnbull <streamweaver@gmail.com>
2021-07-03 17:04:38 -0400

    Adding discord server announcement

---
+++ _posts/2021-07-03-now-with-twenty-percent-more-discord.md
@@ -0,0 +1,20 @@
+---
+layout: post
+title: Discord Channel is Here
+date: 2021-07-03 16:52
+category: Misc
+author: Scott Turnbull
+tags: []
+summary: I've put up a discord server for rpg and 5e rules discussion, advice for games, and other TTRPG Talk.
+---
+
+I just opened a discord server for rules discussions, workshops and the like.  People interested in thoughtful 
+discussions on 5e or TTRPG rules, adventures or more are welcome to sign up. Please note that general rules of 
+good social interactions and good-faith conversations apply. This will likely be a small server but anyone is 
+welcome who wants to get into a deeper conversation than Twitter allows. Please feel free to share this link. 
+The server just opened this weekend and is still getting set-up.
+
+If you're seeing this announcement at a later date just hit me up on Twitter for an invite link.
+
+**Invite Link:** [https://discord.gg/G5jbGsMB](https://discord.gg/G5jbGsMB)
+
--- _scripts/page_description.py
+++ _scripts/page_description.py
@@ -1,84 +1,133 @@
-import urllib.request
-import bs4
-
-# I am in the begining stages of adding some natural language options to the blog and this is just play code.
-
 import nltk
 import string
+import os
 
+from urllib.parse import urlparse
+from nltk.stem import porter
 from nltk.stem.porter import PorterStemmer
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk.parse.generate import generate, demo_grammar
 from bs4 import BeautifulSoup
 
-LOCAL_STOPWORDS = ['road', '-', 'part', 'join', 'one', 'mile', 'try', 'use', 'new', 'useful', 'use', 'may' 'seem', 'like']
+LOCAL_STOPWORDS = [
+    "road",
+    "-",
+    "part",
+    "join",
+    "one",
+    "mile",
+    "try",
+    "use",
+    "new",
+    "useful",
+    "use",
+    "may", 
+    "seem",
+    "like",
+    "way",
+    "get"
+]
+
 
 class PageDescription:
-
-    def __init__(self, url):
+    def __init__(self, url, basedir):
         """
         Pulls the content from a page off of optional rule and provides some NLTK convience methods.
         """
         self.url = url
-        self.raw_page_data = ''
-        self.page_content = ''
-        self.stopwords = []
+        self.basedir = basedir
+        self.raw_page_data = ""
+        self.page_content = ""
+        self.stopwords = extended_stopwords()
         self.clean_tokens = []
-        self._extend_stopwords()
-        self._get_content()
-
-    def _extend_stopwords(self):
-        self.stopwords = stopwords.words('english')
-        self.stopwords.extend(map(str, range(10)))
-        self.stopwords.extend(LOCAL_STOPWORDS)
+        self._set_raw_data()
+        self._set_content()
 
     def freq_distribution(self):
         freq = nltk.FreqDist(self.get_clean_tokens())
         return sorted(freq.most_common(5), key=lambda x: x[1], reverse=True)
 
-
     def get_clean_tokens(self):
-        st = PorterStemmer()
+        sb = SnowballStemmer("english")
         tokenizer = nltk.RegexpTokenizer(r"\w+")
         tokens = tokenizer.tokenize(self.page_content.lower())
-        clean_tokens = [st.stem(t) for t in tokens]
+        clean_tokens = [sb.stem(t) for t in tokens]
 
         for token in tokens:
-            if token in self.stopwords:
-                clean_tokens.remove(st.stem(token))
+            if token in self.sw:
+                clean_tokens.remove(sb.stem(token))
         self.clean_tokens = clean_tokens
         return clean_tokens
 
-
-    def _get_content(self):
-        self._get_webpage()
-        soup = BeautifulSoup(self.raw_page_data, 'html.parser')
-        article = soup.find('article')
+    def _set_content(self):
+        soup = BeautifulSoup(self.raw_page_data, "html.parser")
+        article = soup.find("article")
         if article:
             self.page_content = article.get_text()
 
-    def _get_webpage(self):
-        with urllib.request.urlopen(self.url) as wp:
-            self.raw_page_data = wp.read()
-             
-def get_url_list(filepath):
+    def _set_raw_data(self):
+        with open(self.basedir + urlparse(self.url).path + "index.html", "rb") as f:
+            self.raw_page_data = f.read()
+
+
+class PageCollection:
+    def __init__(self, pages):
+        self.pages = pages
+        self._aggregate_content()
+
+    def _aggregate_content(self):
+        self.content = " ".join([p.page_content for p in self.pages])
+
+    def get_clean_tokens(self):
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        tokens = tokenizer.tokenize(self.content.lower())
+        clean_tokens = [t for t in tokens]
+
+        for token in tokens:
+            if token in extended_stopwords():
+                clean_tokens.remove(token)
+        self.clean_tokens = clean_tokens
+        return clean_tokens
+
+    def freq_distribution(self):
+        freq = nltk.FreqDist(self.get_clean_tokens())
+        return sorted(freq.most_common(10), key=lambda x: x[1], reverse=True)
+
+
+
+def extended_stopwords():
+    sw = stopwords.words("english")
+    sw.extend(map(str, range(10)))
+    sw.extend(LOCAL_STOPWORDS)
+    return sw
+
+
+def get_url_list_from_sitemap(filepath):
     """
     Reads a local sitemap file to pull out the URLs
     """
-    with open(filepath) as f:
-        xml = bs4.BeautifulSoup(f.read(), 'lxml-xml')
-        return [loc.contents[0] for loc in xml.find_all('loc')]
+    with open(filepath, "rb") as f:
+        xml = BeautifulSoup(f.read(), "lxml-xml")
+        return [loc.contents[0] for loc in xml.find_all("loc")]
 
-def page_list(sitemap_path):
-    return [PageDescription(url) for url in get_url_list(sitemap_path)]
+
+def get_post_list(basedir, url_list):
+    post_list = []
+    for url in url_list:
+        if urlparse(url).path.startswith("/202"):
+            post_list.append(PageDescription(url, basedir))
+    return post_list
+
 
 def main(url):
-    page = PageDescription(url)
-    print(page.freq_distribution())
-
+    pass
 
 
 if __name__ == "__main__":
     # main('https://www.optionalrule.com/2021/06/27/gritty-healing-and-survival-rules/')
-    pages = page_list('docs/sitemap.xml')
-    for page in pages:
-        print(page.freq_distribution())
+    post_list = get_post_list("docs/", get_url_list_from_sitemap("docs/sitemap.xml"))
+    pages = PageCollection(post_list)
+    print(", ".join([fq[0] for fq in pages.freq_distribution()]))
